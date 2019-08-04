ba15bbef404e5f3b2660a9baaa91a55f49f14648
Aidan Zealley <aidan@zealley.com>
2019-08-04 16:31:11 +0100

    Update readme

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # strava-toolkit
-Tools and stats for your Strava account
+Tools and stats for your Strava account.
+
+Requires setting up an API application on your Strava account:
+https://www.strava.com/settings/api
 
 `npm install`
 
 `npm run build`
+
+The Client ID and Client Secret are required for the app to obtain an access token.
--- app/activities/all-activities/update-all-activities.js
+++ app/activities/all-activities/update-all-activities.js
@@ -10,6 +10,8 @@ export default function(rootElement, form) {
 
     const requestBody = updateActivity(form);
 
+    const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
+
     async function asyncForEach(array, callback) {
         for (let index = 0; index < array.length; index++) {
             await callback(array[index], index, array);
@@ -18,7 +20,8 @@ export default function(rootElement, form) {
 
     async function start() {
         await asyncForEach(rootElement.view.activitiesData, async (activity, index) => {
-            await updateActivityData(rootElement, requestBody, activity.id)
+            await waitFor(20);
+            // await updateActivityData(rootElement, requestBody, activity.id)
 
             updateProgressBar(progressBar, index + 1, rootElement.view.activitiesData.length, false);
         });
